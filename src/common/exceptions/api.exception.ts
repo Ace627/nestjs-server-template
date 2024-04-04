85024f0981bc8a6f96a1dce006fb144cbff9981e
Legendary <1207588603@qq.com>
2024-04-04 22:24:12 +0800

    feat：全局异常过滤器

---
+++ src/common/exceptions/api.exception.ts
@@ -0,0 +1,24 @@
+/**
+ * @description 自定义异常
+ */
+import { HttpException, HttpStatus } from '@nestjs/common'
+
+export class ApiException extends HttpException {
+  private code: number
+
+  constructor(message: string, code?: number) {
+    // 权限问题一律使用 401 错误码
+    if (code && code === HttpStatus.UNAUTHORIZED) {
+      super(message, HttpStatus.OK)
+      this.code = HttpStatus.UNAUTHORIZED
+    } else {
+      // 其它异常一律使用 500 错误码
+      super(message, code ?? HttpStatus.OK)
+      this.code = code ?? HttpStatus.INTERNAL_SERVER_ERROR
+    }
+  }
+
+  getCode(): number {
+    return this.code
+  }
+}
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,16 +1,19 @@
+import { APP_FILTER, APP_GUARD } from '@nestjs/core'
 import { Global, Module } from '@nestjs/common'
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler'
 import { SharedService } from '@/shared/shared.service'
-import { APP_GUARD } from '@nestjs/core'
+import { AllExceptionsFilter } from '@/common/filters/all-exception.filter'
 
 @Global()
 @Module({
   imports: [
     /* 导入速率限制模块   ttl: 单位毫秒， 表示 ttl 秒内最多只能请求 limit 次， 避免暴力攻击 */
-    ThrottlerModule.forRoot([{ name: 'short', ttl: 1 * 60 * 1000, limit: 60 }]),
+    ThrottlerModule.forRoot([{ name: 'short', ttl: 1 * 60 * 1000, limit: 6 }]),
   ],
   providers: [
     SharedService,
+    /** 全局异常过滤器 */
+    { provide: APP_FILTER, useClass: AllExceptionsFilter },
     /** 速率限制守卫 */
     { provide: APP_GUARD, useClass: ThrottlerGuard },
   ],
