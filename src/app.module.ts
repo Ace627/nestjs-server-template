07c6dc2523f87005a0710d3be2404d5bd16153b5
Legendary <1207588603@qq.com>
2024-04-04 20:43:02 +0800

    feat：公共模块占位文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common'
+import { SharedModule } from '@/shared/shared.module'
 
 @Module({
-  imports: [],
+  imports: [
+    /** 公共模块 */
+    SharedModule,
+  ],
   controllers: [],
   providers: [],
 })
