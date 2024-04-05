6e6f46845859726926a1b9fb37a7ba72b01f1593
Legendary <1207588603@qq.com>
2024-04-05 13:38:43 +0800

    perf：缓存环境变量

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { SharedModule } from '@/shared/shared.module'
-import { LoginModule } from './modules/login/login.module';
+import { LoginModule } from './modules/login/login.module'
 import configuration from './config/configuration'
 
 @Module({
   imports: [
     /** 配置环境变量 http://nestjs.inode.club/techniques/configuration */
-    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true, cache: true }),
     /** 公共模块 */
     SharedModule,
     LoginModule,
